22201377f44c55bd55c15c42a3af3ea31fa76eec
daxitzadafiya <17bmiit040@gmail.com>
2020-08-31 22:56:36 +0530

    Updates

---
+++ precache-manifest.635399cdc9f28a4985eff4628366ab39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06fed3dc4b29282d37210beba0b45bd",
+    "url": "/news-reader/index.html"
+  },
+  {
+    "revision": "fa880c2ee534d2f54ad3",
+    "url": "/news-reader/static/css/main.9bb4dcee.chunk.css"
+  },
+  {
+    "revision": "7748571dac7093365586",
+    "url": "/news-reader/static/js/2.f0017c42.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/news-reader/static/js/2.f0017c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa880c2ee534d2f54ad3",
+    "url": "/news-reader/static/js/main.40cb5bad.chunk.js"
+  },
+  {
+    "revision": "16842a36d6f76181206a",
+    "url": "/news-reader/static/js/runtime-main.d13d1d61.js"
+  }
+]);
